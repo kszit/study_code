29a9153e02e36ed9feca7206ab01316d6bcf9d97
hanxiaowei <kszit@163.com>
2015-06-27 13:09:17 +0800

    修改为maven父子结构

---
+++ stu_util/src/test/java/com/kszit/util/jdbc/UpdateTest.java
@@ -0,0 +1,25 @@
+package com.kszit.util.jdbc;
+
+import org.junit.Test;
+
+import com.kszit.util.jdbc.oracle.OracleConnection;
+
+public class UpdateTest {
+
+	@Test
+	public void update() throws Exception{
+//		IConnection con = new OracleConnection(
+//				"172.31.201.151",
+//				"1521",
+//				"BDCOR2",
+//				"bdcor",
+//				"develop2015");
+//		
+//		DbHandler dbhandler = new DbHandler(con.connection());
+//		
+//		dbhandler.update("update PIP_COMM_DICT_VERSION set dict_version='3' where dict_type='5'");
+//		
+//		dbhandler.closeCon();
+		
+	}
+}
