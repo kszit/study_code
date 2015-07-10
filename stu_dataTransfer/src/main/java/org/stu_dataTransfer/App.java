6ceb2585e7b9323ecdf6bd824e0a891676f87359
hanxiaowei <kszit@163.com>
2015-07-10 16:38:32 +0800

    添加dataTransfer组件；

---
+++ stu_dataTransfer/src/main/java/org/stu_dataTransfer/App.java
@@ -0,0 +1,13 @@
+package org.stu_dataTransfer;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
